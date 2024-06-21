627c9eef6b33d33a17e0bc0a99655a3d6324a0f2
Katherina-Ushachov <katherina.ushachov@yahoo.de>
2024-06-21 11:21:56 +0200

    Eingabefeld?

--- src/app/identify/identify.component.ts
+++ src/app/identify/identify.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-identify',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './identify.component.html',
   styleUrl: './identify.component.css'
 })
 export class IdentifyComponent {
+    username = '';
 
 }
