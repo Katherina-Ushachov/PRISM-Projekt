329604b609d41753faaa04053cc14eb22d9cbb9f
Katherina-Ushachov <katherina.ushachov@yahoo.de>
2024-06-21 11:14:34 +0200

    Component erstellt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MyButtonComponentComponent } from './my-button-component/my-button-component.component';
+import {IdentifyComponent} from './identify/identify.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MyButtonComponentComponent],
+  imports: [RouterOutlet, MyButtonComponentComponent, IdentifyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/identify/identify.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-identify',
+  standalone: true,
+  imports: [],
+  templateUrl: './identify.component.html',
+  styleUrl: './identify.component.css'
+})
+export class IdentifyComponent {
+
+}
