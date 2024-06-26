af80cdf8cd7183141e1f2a39ee0014d75481c8c7
Katherina-Ushachov <katherina.ushachov@yahoo.de>
2024-06-26 09:19:33 +0200

    ?

--- src/app/riddle/riddle.component.ts
+++ src/app/riddle/riddle.component.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
   selector: 'app-riddle',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [],
   templateUrl: './riddle.component.html',
   styleUrl: './riddle.component.css'
 })
 export class RiddleComponent {
 profileForm: any;
 start(){
-  this.profileForm = new FormGroup({
-    solution: new FormControl('', Validators.required)
-  });
+
 }
 }
