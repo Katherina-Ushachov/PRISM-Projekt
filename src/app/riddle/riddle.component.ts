1d7477a9ac5738fa3aaacfde9fbefab20282a79f
Katherina-Ushachov <katherina.ushachov@yahoo.de>
2024-06-27 15:55:07 +0200

    Versuche mit Validierung

--- src/app/riddle/riddle.component.ts
+++ src/app/riddle/riddle.component.ts
@@ -1,18 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators,ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 
 
 @Component({
   selector: 'app-riddle',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './riddle.component.html',
   styleUrl: './riddle.component.css'
 })
 export class RiddleComponent {
   profileForm = new FormGroup({
-    solution:new FormControl('',Validators.required)
+    solution:new FormControl('',[
+      Validators.required, 
+      Validators.minLength(3),
+forbiddenNameValidator(/rot|gruen|grün|blau|gelb|hellblau|türkis|tuerkis|lila|violett|orange|purpur|dunkelblau)
+    ])
   });
   value: boolean = false;
+}
+ function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const forbidden = !nameRe.test(control.value);
+    return forbidden ? {forbiddenName: {value: control.value}} : null;
+  };
 }
 
