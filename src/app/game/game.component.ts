ead19ed30e152b545a4c73b1ace899f57b2b7269
Katherina-Ushachov <katherina.ushachov@yahoo.de>
2024-07-05 12:05:57 +0200

    Die Softwareseite der ... Seite ist abgeschlossen.

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -8,153 +8,70 @@ import { Component } from '@angular/core';
   styleUrl: './game.component.css'
 })
 export class GameComponent {
+  purple: number = -1;
+  purpleLabel: Array<string> = ["Lila", "Violet", "Lavendar", "Mauve"]
+
+  blue: number = -1;
+  blueLabel: Array<string> = ["Indigo", "Azul", "Livia", "Sini"]
+
+  cerulean:number = -1;
+  ceruleanLabel: Array<string> = ["Cerulean","Aqua","Kaltrina","Sunil"]
+
+  green:number = -1;
+  greenLabel: Array<string> = ["Chloe","Emerald","Olive","Midori"]
+
+  yellow:number = -1;
+  yellowLabel: Array<string> = ["Amber","Blain","Flavian","Rozerin"]
+
+  orange:number = -1;
+  orangeLabel: Array<string> = ["Clementine","Alani","Peach","Gintaras"]
+
+  red:number = -1;
+ redLabel: Array<string> = ["Ginger","Ruby","Scarlet","Adam"]
 
 
-  purple1: boolean = false;
-  purple2: boolean = false;
-  purple3: boolean = false;
-  purple4: boolean = false;
 
-  blue1: boolean = false;
-  blue2: boolean = false;
-  blue3: boolean = false;
-  blue4: boolean = false;
-
-  cerulean1: boolean = false;
-  cerulean2: boolean = false;
-  cerulean3: boolean = false;
-  cerulean4: boolean = false;
-
-  green1: boolean = false;
-  green2: boolean = false;
-  green3: boolean = false;
-  green4: boolean = false;
-
-  yellow1: boolean = false;
-  yellow2: boolean = false;
-  yellow3: boolean = false;
-  yellow4: boolean = false;
-
-  orange1: boolean = false;
-  orange2: boolean = false;
-  orange3: boolean = false;
-  orange4: boolean = false;
-
-  red1: boolean = false;
-  red2: boolean = false;
-  red3: boolean = false;
-  red4: boolean = false;
-
-  selectPurple1() {
-    this.purple1 = !this.purple1;
+  selectPurple(i: number) {
+    if (this.purple != i) {
+      this.purple = i;
+    }
   }
 
-  selectPurple2() {
-    this.purple2 = !this.purple2;
+
+  selectBlue(i: number) {
+    if (this.blue != i) { 
+      this.blue = i 
+    }
   }
 
-  selectPurple3() {
-    this.purple3 = !this.purple3;
+
+  selectCerulean(i: number) {
+    if (this.cerulean != i) { 
+      this.cerulean = i 
+    }
   }
 
-  selectPurple4() {
-    this.purple4 = !this.purple4;
+
+  selectGreen(i: number) {
+    if (this.green != i) { 
+      this.green = i 
+    }  }
+
+  selectYellow(i: number) {
+    if (this.yellow != i) { 
+      this.yellow = i 
+    } 
   }
 
-  selectBlue1() {
-    this.blue1 = !this.blue1;
+  selectOrange(i: number) {
+    if (this.orange != i) { 
+      this.orange = i 
+    } 
   }
 
-  selectBlue2() {
-    this.blue2 = !this.blue2;
-
-  }
-
-  selectBlue3() {
-    this.blue3 = !this.blue3;
-  }
-
-  selectBlue4() {
-    this.blue4 = !this.blue4;
-  }
-
-  selectCerulean1() {
-    this.cerulean1 = !this.cerulean1;
-  }
-
-  selectCerulean2() {
-    this.cerulean2 = !this.cerulean2;
-  }
-
-  selectCerulean3() {
-    this.cerulean3 = !this.cerulean3;
-  }
-
-  selectCerulean4() {
-    this.cerulean4 = !this.cerulean4;
-  }
-
-  selectGreen1() {
-    this.green1 = !this.green1;
-  }
-
-  selectGreen2() {
-    this.green2 = !this.green2;
-  }
-
-  selectGreen3() {
-    this.green3 = !this.green3;
-  }
-
-  selectGreen4() {
-    this.green4 = !this.green4;
-  }
-
-  selectYellow1() {
-    this.yellow1 = !this.yellow1;
-  }
-
-  selectYellow2() {
-    this.yellow2 = !this.yellow2;
-  }
-
-  selectYellow3() {
-    this.yellow3 = !this.yellow3;
-  }
-
-  selectYellow4() {
-    this.yellow4 = !this.yellow4;
-  }
-
-  selectOrange1() {
-    this.orange1 = !this.orange1;
-  }
-
-  selectOrange2() {
-    this.orange2 = !this.orange2;
-  }
-
-  selectOrange3() {
-    this.orange3 = !this.orange3;
-  }
-
-  selectOrange4() {
-    this.orange4 = !this.orange4;
-  }
-
-  selectRed1() {
-    this.red1 = !this.red1;
-  }
-
-  selectRed2() {
-    this.red2 = !this.red2;  
-  }
-
-  selectRed3() {
-    this.red3 = !this.red3;
-  }
-
-  selectRed4() {
-    this.red4 = !this.red4;
+  selectRed(i: number) {
+    if (this.red != i) { 
+      this.red = i 
+    } 
   }
 }
