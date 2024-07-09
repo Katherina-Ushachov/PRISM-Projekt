279b309f21af0a93a04bb9a3f5342a186a2305d2
Katherina-Ushachov <katherina.ushachov@yahoo.de>
2024-07-09 15:40:02 +0200

    Schlusscommit für heute

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,34 +2,47 @@ import { Component } from '@angular/core';
 import { SchlusselementComponent } from "../schlusselement/schlusselement.component";
 
 @Component({
-    selector: 'app-game',
-    standalone: true,
-    templateUrl: './game.component.html',
-    styleUrl: './game.component.css',
-    imports: [SchlusselementComponent]
+  selector: 'app-game',
+  standalone: true,
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.css',
+  imports: [SchlusselementComponent]
 })
 export class GameComponent {
+[x: string]: any;
+
+
+
   purple: number = -1;
   purpleLabel: Array<string> = ["Lila", "Violet", "Lavendar", "Mauve"]
 
   blue: number = -1;
   blueLabel: Array<string> = ["Indigo", "Azul", "Livia", "Sini"]
 
-  cerulean:number = -1;
-  ceruleanLabel: Array<string> = ["Cerulean","Aqua","Kaltrina","Sunil"]
+  cerulean: number = -1;
+  ceruleanLabel: Array<string> = ["Cerulean", "Aqua", "Kaltrina", "Sunil"]
 
-  green:number = -1;
-  greenLabel: Array<string> = ["Chloe","Emerald","Olive","Midori"]
+  green: number = -1;
+  greenLabel: Array<string> = ["Chloe", "Emerald", "Olive", "Midori"]
 
-  yellow:number = -1;
-  yellowLabel: Array<string> = ["Amber","Blain","Flavian","Rozerin"]
+  yellow: number = -1;
+  yellowLabel: Array<string> = ["Amber", "Blain", "Flavian", "Rozerin"]
 
-  orange:number = -1;
-  orangeLabel: Array<string> = ["Clementine","Alani","Peach","Gintaras"]
+  orange: number = -1;
+  orangeLabel: Array<string> = ["Clementine", "Alani", "Peach", "Gintaras"]
 
-  red:number = -1;
- redLabel: Array<string> = ["Ginger","Ruby","Scarlet","Adam"]
+  red: number = -1;
+  redLabel: Array<string> = ["Ginger", "Ruby", "Scarlet", "Adam"]
 
+
+  randomPurpleResult(length: number) {
+    let purpleResults = ["Ihre violette Begleitung heißt {{purpleLabel[purple]}}. {{purpleLabel[purple]}} ist innerhalb der P.R.I.S.M.-Virtualität dafür zuständig, Euch zu stabilisieren. Die meisten Personen sind nach dem Upload in die P.R.I.S.M.-Virtualität zunächst desorientiert und verunsichert. Sollte dies auch bei Ihnen der Fall sein, wird {{purpleLabel[purple]}} Sie begrüßen und die für Sie individuell berechnete Virtualität zeigen. In der Regel besteht diese aus einem Gemeinschaftsraum, in dem Zeit mit den NPCs verbracht wird, einem Rückzugsort und einem esonderten Raum, dessen Zweck Sie in der Präsentation erfahren werden. <br> Auch nach der Begrüßungssequenz ist dies der wichtigste Zweck von {{ purpleLabel[purple]}} - Stabilität und innere Ruhe zu gewährleisten.Aber {{ purpleLabel[purple]}} ist auch die Person mit der extravagantesten Garderobe und einem überschwänglichen Gemüt - perfekt für ein Begrüßungskommitee! < /p>", "Test-Text 1", "Text 2", "Beispiel"]
+    let result = '';
+    for (let i = 0; i < purpleResults.length; i++) {
+      result += purpleResults[(Math.floor(Math.random() * purpleResults.length))];
+    }
+    return result;
+  }
 
 
   selectPurple(i: number) {
@@ -40,39 +53,40 @@ export class GameComponent {
 
 
   selectBlue(i: number) {
-    if (this.blue != i) { 
-      this.blue = i 
+    if (this.blue != i) {
+      this.blue = i
     }
   }
 
 
   selectCerulean(i: number) {
-    if (this.cerulean != i) { 
-      this.cerulean = i 
+    if (this.cerulean != i) {
+      this.cerulean = i
     }
   }
 
 
   selectGreen(i: number) {
-    if (this.green != i) { 
-      this.green = i 
-    }  }
+    if (this.green != i) {
+      this.green = i
+    }
+  }
 
   selectYellow(i: number) {
-    if (this.yellow != i) { 
-      this.yellow = i 
-    } 
+    if (this.yellow != i) {
+      this.yellow = i
+    }
   }
 
   selectOrange(i: number) {
-    if (this.orange != i) { 
-      this.orange = i 
-    } 
+    if (this.orange != i) {
+      this.orange = i
+    }
   }
 
   selectRed(i: number) {
-    if (this.red != i) { 
-      this.red = i 
-    } 
+    if (this.red != i) {
+      this.red = i
+    }
   }
 }
