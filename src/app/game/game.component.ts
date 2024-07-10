db4eafdefc0ba5ffebbb2395fb5c7704226bd0f9
Katherina-Ushachov <katherina.ushachov@yahoo.de>
2024-07-10 14:04:53 +0200

    Neuer Versuch

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -15,6 +15,8 @@ import { RandomService } from '../random.service';
 export class GameComponent {
   constructor(randomService: RandomService) { }
 
+  callService(){}
+
 [x: string]: any;
  
 
@@ -40,7 +42,7 @@ export class GameComponent {
   redLabel: Array<string> = ["Ginger", "Ruby", "Scarlet", "Adam"]
 
 
-  
+
 
 
   selectPurple(i: number) {
@@ -87,4 +89,15 @@ export class GameComponent {
       this.red = i
     }
   }
+
+  randomPurpleResult(length: number) {
+    
+    let purpleResults = [`Ihre violette Begleitung heißt ${this.purpleLabel[this.purple]} . ${this.purpleLabel[this.purple]} ist innerhalb der P.R.I.S.M.-Virtualität dafür zuständig, Euch zu stabilisieren. Die meisten Personen sind nach dem Upload in die P.R.I.S.M.-Virtualität zunächst desorientiert und verunsichert. Sollte dies auch bei Ihnen der Fall sein, wird ${this.purpleLabel[this.purple]}  Sie begrüßen und die für Sie individuell berechnete Virtualität zeigen. In der Regel besteht diese aus einem Gemeinschaftsraum, in dem Zeit mit den NPCs verbracht wird, einem Rückzugsort und einem gesonderten Raum, dessen Zweck Sie in der Präsentation erfahren werden. <br> Auch nach der Begrüßungssequenz ist dies der wichtigste Zweck von ${this.purpleLabel[this.purple]}- Stabilität und innere Ruhe zu gewährleisten. Aber ${this.purpleLabel[this.purple]}} ist auch die Person mit der extravagantesten Garderobe und einem überschwänglichen Gemüt - perfekt für ein Begrüßungskommitee! `, `Test-Text 1  ${this.purpleLabel[this.purple]}`, `Text 2  ${this.purpleLabel[this.purple]} `,`Beispiel  ${this.purpleLabel[this.purple]}`]
+    let resultpurple = '';
+    for (let i = 0; i < purpleResults.length; i++) {
+      resultpurple += purpleResults[(Math.floor(Math.random() * purpleResults.length))];
+    }
+    return resultpurple;
+  }
+
 }
